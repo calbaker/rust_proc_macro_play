078db372e7eb38f4092df069c5b7ecee29792cb6
Chad Baker <chad.baker@nrel.gov>
2022-05-03 09:21:47 -0600

    structured in three crates

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,22 +1,12 @@
 // much of this code is bowrrowed from https://blog.logrocket.com/procedural-macros-in-rust/
 
-use proc_macro::DoubleF64;
+use proc_macros::DoubleF64;
 
 #[derive(DoubleF64)]
 struct MyStruct {
     my_string: String,
     my_number: f64,
     my_other_number: f64,
-}
-
-fn main() {
-    let mystruct = MyStruct {
-        my_string: "some str".to_string(),
-        my_number: 2.0,
-        my_other_number: 17.0,
-    };
-    println!("my_number * 2: {}", mystruct.double_my_number());
-    println!("my_other_number * 2: {}", mystruct.double_my_other_number());
 }
 
 #[cfg(test)]
